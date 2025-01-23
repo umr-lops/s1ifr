3e23407966113fcfa4f5caaf287b5078aaf28a65
agrouaze <antoine.grouazel@ifremer.fr>
2025-01-23 11:12:49 +0000

    add a script to use singularity image for sorting S1 product

---
+++ s1ifr/scripts/example_singularity_image_usage.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+# sort WV data
+img=/home/datawork-cersat-public/cache/project/mpc-sentinel1/workspace/singularity/s1ifr/s1ifr.sif
+#cmd="micromamba activate /opt/app-s1ifr/envs/envs1ifr && archivesafe -s S1C --mode WV -p OCN_"
+#cmd="micromamba activate /opt/app-s1ifr/envs/envs1ifr && archivesafe -h"
+#cmd="archivesafe -h"
+#cmd='/home1/datahome/agrouaze/sources/git/s1ifr/payload_sorting_S1_product.bash'
+cmd='/opt/app-s1ifr/envs/envs1ifr/bin/archivesafe --input-safe '
+safe=$1
+echo 'safe to sort: '$safe
+echo 'command '$cmd
+echo 'img '$img
+singularity run --bind /home/datawork-cersat-public/ --bind /home1/datahome/ --bind /home1/scratch/ $img $cmd $safe
+echo done
